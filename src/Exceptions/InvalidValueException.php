f11af16adceb711bf607a76f3df54f0258fd1a11
Christian Ramelow <info@codekandis.net>
2017-08-26 20:56:13 +0200

    Adds all necessary exceptions.

---
+++ src/Exceptions/InvalidValueException.php
@@ -0,0 +1,39 @@
+<?php declare( strict_types = 1 );
+
+namespace CodeKandis\Phlags\Exceptions
+{
+
+    /**
+     * Represents an exception if a value passed to the flagable is invalid.
+     * @package codekandis\phlags
+     * @author  Christian Ramelow <info@codekandis.net>
+     */
+    class InvalidValueException extends \RuntimeException implements ErrorMessagesExceptionInterface
+    {
+        /**
+         * Stores the error messages of the exception.
+         * @var string[]
+         */
+        private $_errorMessages = [];
+
+        /**
+         * {@inheritdoc}
+         * @see ErrorMessagesExceptionInterface::getErrorMessages()
+         */
+        public function getErrorMessages(): array
+        {
+            return $this->_errorMessages;
+        }
+
+        /**
+         * {@inheritdoc}
+         * @see ErrorMessagesExceptionInterface::withErrorMessages()
+         */
+        public function withErrorMessages( array $errorMessages ): ErrorMessagesExceptionInterface
+        {
+            $this->_errorMessages = $errorMessages;
+
+            return $this;
+        }
+    }
+}
---
+++ src/Exceptions/UnsupportedOperationException.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types = 1 );
+
+namespace CodeKandis\Phlags\Exceptions
+{
+
+    /**
+     * Represents an exception if an invalid member has been accessed.
+     * @package codekandis\phlags
+     * @author  Christian Ramelow <info@codekandis.net>
+     */
+    class UnsupportedOperationException extends \LogicException
+    {
+    }
+}
