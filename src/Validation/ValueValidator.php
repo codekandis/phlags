ec14d2246392599b62fc3456b8e7c199d13d76c4
Christian Ramelow <info@codekandis.net>
2017-08-28 17:29:47 +0200

    Refactors the inperformant validation.

--- src/Validation/ValueValidator.php
+++ src/Validation/ValueValidator.php
@@ -1,47 +1,44 @@
-<?php declare( strict_types = 1 );
+<?php declare(strict_types=1);
 
 namespace CodeKandis\Phlags\Validation
 {
 
-    use CodeKandis\Phlags\FlagableInterface;
-    use CodeKandis\Phlags\Validation\Results\ValidationResultInterface;
-    use CodeKandis\Phlags\Validation\Results\ValueValidationResult;
+	use CodeKandis\Phlags\FlagableInterface;
+	use CodeKandis\Phlags\Validation\Results\ValidationResultInterface;
+	use CodeKandis\Phlags\Validation\Results\ValueValidationResult;
 
-    /**
-     * Represents the validator of all flagables.
-     * @package codekandis\phlags
-     * @author  Christian Ramelow <info@codekandis.net>
-     */
-    final class ValueValidator implements ValueValidatorInterface
-    {
-        /**
-         * {@inheritdoc}
-         * @see ValueValidatorInterface::validate()
-         */
-        public function validate( FlagableInterface $flagable, int $maxValue, $value ): ValidationResultInterface
-        {
-            $errorMessages = [];
-            if ( $value instanceof $flagable === false && ( is_int( $value ) === false || $value < 0 ) )
-            {
-                $errorMessages[] = sprintf(
-                    "Invalid type in value. Unsigned 'int' or instance of '%s' expected.",
-                    get_class( $flagable )
-                );
-            }
-            else
-            {
-                $flagValue = is_int( $value ) === true ? $value : $value->getValue();
-                if ( $flagValue > $maxValue )
-                {
-                    $errorMessages[] = sprintf(
-                        "The value '%s' exceeds the maximum flag value of '%s'.",
-                        (string)$flagValue,
-                        (string)$maxValue
-                    );
-                }
-            }
+	/**
+	 * Represents the validator of all flagables.
+	 *
+	 * @package codekandis\phlags
+	 * @author  Christian Ramelow <info@codekandis.net>
+	 */
+	final class ValueValidator implements ValueValidatorInterface
+	{
+		/**
+		 * {@inheritdoc}
+		 * @see ValueValidatorInterface::validate()
+		 */
+		public function validate( FlagableInterface $flagable, int $maxValue, $value ): ValidationResultInterface
+		{
+			$errorMessages = [];
+			if ( $value instanceof $flagable === false && (is_int( $value ) === false || $value < 0) )
+			{
+				$errorMessages[] = sprintf(
+					"Invalid type in value. Unsigned 'int' or instance of '%s' expected.",
+					get_class( $flagable )
+				);
+			}
+			if ( is_int( $value ) && $value > $maxValue )
+			{
+				$errorMessages[] = sprintf(
+					"The value '%s' exceeds the maximum flag value of '%s'.",
+					(string)$value,
+					(string)$maxValue
+				);
+			}
 
-            return new ValueValidationResult( $errorMessages );
-        }
-    }
+			return new ValueValidationResult( $errorMessages );
+		}
+	}
 }
