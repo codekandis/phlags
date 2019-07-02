37c049995edb40a4fdb2ba78d5df3bd718b84695
Christian Ramelow <info@codekandis.net>
2019-07-02 13:18:23 +0200

    Fixes the inheritance in the invalid flagable exception.

--- src/Exceptions/InvalidFlagableException.php
+++ src/Exceptions/InvalidFlagableException.php
@@ -1,14 +1,14 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Phlags\Exceptions;
 
-use LogicException;
+use RuntimeException;
 
 /**
  * Represents an exception if the validation of the flagable has been failed.
  * @package codekandis/phlags
  * @author  Christian Ramelow <info@codekandis.net>
  */
-class InvalidFlagableException extends LogicException implements ErrorMessagesExceptionInterface
+class InvalidFlagableException extends RuntimeException implements ErrorMessagesExceptionInterface
 {
 	/**
 	 * Stores the error messages of the exception.
