a85c385df12cdfedaf791531cbf5c5ca5fe5bb13
Christian Ramelow <info@codekandis.net>
2019-07-03 20:43:39 +0200

    Refactors the flagable validator interface.
    - inherits the validator interface
    - removes the usage of the flagable validation result

--- src/Validation/FlagableValidatorInterface.php
+++ src/Validation/FlagableValidatorInterface.php
@@ -1,20 +1,23 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Phlags\Validation;
 
-use CodeKandis\Phlags\Validation\Results\FlagableValidationResultInterface;
-
 /**
  * Represents the interface of all validators validating flagables.
  * @package codekandis/phlags
  * @author Christian Ramelow <info@codekandis.net>
  */
-interface FlagableValidatorInterface
+interface FlagableValidatorInterface extends ValidatorInterface
 {
+	/**
+	 * Gets the maximum flag value of the validated flagable.
+	 * @return int The maximum flag value of the validated flagable.
+	 */
+	public function getMaxValue(): int;
+
 	/**
 	 * Validates the flagable.
 	 * @param string $flagableClassName The class name of the flagable to validate.
 	 * @param array $reflectedFlags The reflected flags of the flagable to validate.
-	 * @return FlagableValidationResultInterface The result of the flagable validation.
 	 */
-	public function validate( string $flagableClassName, array $reflectedFlags ): FlagableValidationResultInterface;
+	public function validate( string $flagableClassName, array $reflectedFlags ): void;
 }
