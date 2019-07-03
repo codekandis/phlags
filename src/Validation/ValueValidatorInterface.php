cc3773d050755ce5c9b22bd3d702afd033672637
Christian Ramelow <info@codekandis.net>
2019-07-03 21:10:12 +0200

    Refactors the value validator interface.
    - inherits the validator interface
    - removes the usage of the value validation result

--- src/Validation/ValueValidatorInterface.php
+++ src/Validation/ValueValidatorInterface.php
@@ -2,14 +2,13 @@
 namespace CodeKandis\Phlags\Validation;
 
 use CodeKandis\Phlags\FlagableInterface;
-use CodeKandis\Phlags\Validation\Results\ValidationResultInterface;
 
 /**
  * Represents the interface of all validators validating if a value is a valid flag.
  * @package codekandis/phlags
  * @author Christian Ramelow <info@codekandis.net>
  */
-interface ValueValidatorInterface
+interface ValueValidatorInterface extends ValidatorInterface
 {
 	/**
 	 * Validates the value.
@@ -17,7 +16,6 @@ interface ValueValidatorInterface
 	 * @param array $reflectedFlags The reflected flags of the flagable.
 	 * @param int $maxValue The maximum value of the flagable.
 	 * @param int|string|FlagableInterface $value The value to validate.
-	 * @return ValidationResultInterface The result of the value validation.
 	 */
-	public function validate( FlagableInterface $flagable, array $reflectedFlags, int $maxValue, $value ): ValidationResultInterface;
+	public function validate( FlagableInterface $flagable, array $reflectedFlags, int $maxValue, $value ): void;
 }
