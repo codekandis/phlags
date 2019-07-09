4dc5a269de184e7f943c28aee7b76b05718a8c90
Christian Ramelow <info@codekandis.net>
2019-07-09 13:52:31 +0200

    Adds the `FlagableState` implementing the `FlagableStateInterface`.

---
+++ src/FlagableState.php
@@ -0,0 +1,127 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Phlags;
+
+use CodeKandis\Phlags\Validation\InvalidFlagableException;
+use CodeKandis\Phlags\Validation\ValueValidatorInterface;
+
+/**
+ * Represents a flagable state.
+ * @package codekandis/phlags
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FlagableState implements FlagableStateInterface
+{
+	/**
+	 * Stores if the flagable has been validated.
+	 * @var bool
+	 */
+	private $hasBeenValidated = false;
+
+	/**
+	 * Stores the thrown exception of the validation of the flagable.
+	 * @var null|InvalidFlagableException
+	 */
+	private $validationException;
+
+	/**
+	 * Stores the reflected flags of the flagable.
+	 * @var string[]
+	 */
+	private $reflectedFlags = [];
+
+	/**
+	 * Stores the maximum value of the flagable.
+	 * @var int
+	 */
+	private $maxValue = FlagableInterface::NONE;
+
+	/**
+	 * Stores the value validator of the flagable.
+	 * @var null|ValueValidatorInterface
+	 */
+	private $valueValidator;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getHasBeenValidated(): bool
+	{
+		return $this->hasBeenValidated;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setHasBeenValidated( bool $hasBeenValidated ): void
+	{
+		$this->hasBeenValidated = $hasBeenValidated;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getValidationException(): ?InvalidFlagableException
+	{
+		return $this->validationException;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setValidationException( ?InvalidFlagableException $validationException ): void
+	{
+		$this->validationException = $validationException;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getReflectedFlags(): ?array
+	{
+		return $this->reflectedFlags;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setReflectedFlags( ?array $reflectedFlags ): void
+	{
+		if ( null !== $reflectedFlags )
+		{
+			asort( $reflectedFlags );
+		}
+		$this->reflectedFlags = $reflectedFlags;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getMaxValue(): ?int
+	{
+		return $this->maxValue;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setMaxValue( ?int $maxValue ): void
+	{
+		$this->maxValue = $maxValue;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getValueValidator(): ?ValueValidatorInterface
+	{
+		return $this->valueValidator;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setValueValidator( ?ValueValidatorInterface $valueValidator ): void
+	{
+		$this->valueValidator = $valueValidator;
+	}
+}
