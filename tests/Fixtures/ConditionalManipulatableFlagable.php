e673b096ad1b65b48a3d0ff14a713d39d8045d09
Christian Ramelow <info@codekandis.net>
2019-07-10 13:54:03 +0200

    Refactors the fixture `ConditionalManipulatableFlagable`.

--- tests/Fixtures/ConditionalManipulatableFlagable.php
+++ tests/Fixtures/ConditionalManipulatableFlagable.php
@@ -3,13 +3,14 @@ namespace CodeKandis\Phlags\Tests\Fixtures;
 
 use CodeKandis\Phlags\AbstractFlagable;
 use CodeKandis\Phlags\TraitfulExtensions\ConditionalManipulationExtension;
+use CodeKandis\Phlags\TraitfulExtensions\ConditionalManipulationInterface;
 
 /**
  * Represents a test fixture of a conditional manipulatable flagable.
  * @package codekandis/phlags
  * @author Christian Ramelow <info@codekandis.net>
  */
-class ConditionalManipulatableFlagable extends AbstractFlagable
+class ConditionalManipulatableFlagable extends AbstractFlagable implements ConditionalManipulationInterface
 {
 	use ConditionalManipulationExtension;
 
