6af6593f1936b5809c3bd3b6bd639a1ee43240b5
Christian Ramelow <info@codekandis.net>
2019-07-10 13:47:23 +0200

    Adds the `ConditionalManipulationInterface`.

---
+++ src/TraitfulExtensions/ConditionalManipulationInterface.php
@@ -0,0 +1,31 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Phlags\TraitfulExtensions;
+
+use CodeKandis\Phlags\FlagableInterface;
+
+interface ConditionalManipulationInterface
+{
+	/**
+	 * Sets a flag if the passed condition is true.
+	 * @param int|string|FlagableInterface $value The flag to set.
+	 * @param bool $condition The condition defining if the flag can be set.
+	 * @return self
+	 */
+	public function ifSet( $value, bool $condition ): ConditionalManipulationInterface;
+
+	/**
+	 * Unsets a flag if the passed condition is true.
+	 * @param int|string|FlagableInterface $value The flag to unset.
+	 * @param bool $condition The condition defining if the flag can be unset.
+	 * @return self
+	 */
+	public function ifUnset( $value, bool $condition ): ConditionalManipulationInterface;
+
+	/**
+	 * Switches a flag if the passed condition is true.
+	 * @param int|string|FlagableInterface $value The flag to switch.
+	 * @param bool $condition The condition defining if the flag can be switched.
+	 * @return self
+	 */
+	public function ifSwitch( $value, bool $condition ): ConditionalManipulationInterface;
+}
