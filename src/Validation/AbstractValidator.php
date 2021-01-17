75116d3e53718d352da0964eee94ca0570a7f08a
Christian Ramelow <info@codekandis.net>
2021-01-17 02:18:26 +0100

    Updates the class `AbstractValidator`.

--- src/Validation/AbstractValidator.php
+++ src/Validation/AbstractValidator.php
@@ -1,13 +1,18 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Phlags\Validation;
 
+/**
+ * Represents the base class of all validators.
+ * @package codekandis/phlags
+ * @author Christian Ramelow <info@codekandis.net>
+ */
 abstract class AbstractValidator implements ValidatorInterface
 {
 	/**
 	 * Stores the error messages of the validation.
 	 * @var string[]
 	 */
-	protected $errorMessages = [];
+	protected array $errorMessages = [];
 
 	/**
 	 * {@inheritdoc}
